d10816d808c2d1e331b5a6d0158a3a536f9af8db
Akash <akash922001@gmail.com>
2022-12-21 10:29:56 +0530

    sending servletdemo 1

---
+++ servletdemo1/src/main/java/com/cdac/demo/Demo.java
@@ -0,0 +1,36 @@
+package com.cdac.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo
+ */
+@WebServlet("/getingdemo")
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	response.setContentType("text/html");
+	PrintWriter out = response.getWriter();
+	out.print("<title>CDAC</title>");
+	out.print("<h1><marquee>Welcome to cdac </marquee></h1>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
